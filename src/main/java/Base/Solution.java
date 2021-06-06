2e78ecdb0dbceb3f1727a47ac2825ecf914ad377
Ivedent Lloyd III <ivedent.lloyd@gmail.com>
2021-06-06 16:56:56 -0400

    Initial commit

---
+++ src/main/java/Base/Solution.java
@@ -0,0 +1,94 @@
+package Base;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution 20
+ *  Copyright 2021 Ivedent Lloyd III
+ */
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Solution {
+
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        double tax = 1.0;
+        double totalTax = 0.0;
+
+        double orderAmount = orderInput();
+
+        System.out.print("What is the state? ");
+        String State = in.next();
+
+        aa:
+
+        if (State.equals("Wisconsin")) {
+
+            System.out.print("What is the county? ");
+            String county = in.next();
+
+            if (county.equals("Eau Claire")){
+
+                tax = tax * 0.0555;
+                totalTax = orderAmount * tax;
+                orderAmount = orderAmount + totalTax;
+
+            }
+
+            else if (county.equals("Dane")){
+
+                tax = tax * 0.0554;
+                totalTax = orderAmount * tax;
+                orderAmount = orderAmount + totalTax;
+
+            }
+        }
+
+        else if (State.equals("Illinois")) {
+
+            tax = tax * 0.08;
+            totalTax = orderAmount * tax;
+            orderAmount = orderAmount + totalTax;
+
+        }
+
+        else{
+
+            break aa;
+
+        }
+
+        double roundTax = totalTax * 1000.0;
+        int ceilTax = (int) Math.ceil(roundTax);
+        double finalTax = ceilTax / 1000.0;
+
+        double orderRound = orderAmount * 1000.0;
+        int roundCost = (int) Math.ceil(orderRound);
+        double costFinal = roundCost / 1000.0;
+
+        DecimalFormat format = new DecimalFormat("0.00");
+
+        System.out.print("The tax is $");
+        System.out.print(format.format(finalTax));
+        System.out.println(".");
+
+        DecimalFormat format2 = new DecimalFormat("0.00");
+
+        System.out.print("The total is $");
+        System.out.print(format2.format(costFinal));
+        System.out.print(".");
+
+    }
+
+    private static int orderInput() {
+
+        System.out.print("What is the order amount? ");
+        int orderInput = in.nextInt();
+
+        return orderInput;
+
+    }
+
+}
